9a7188f837891a82b2bd73c3f1f65a9d0e53b418
mauricio <mauriciomorales0410@gmail.com>
2015-10-25 19:09:11 -0300

    inicio-new

--- inicio.php
+++ inicio.php
@@ -1,23 +1,46 @@
+<head>
+    <title>
+        INICIO
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="" type="text/css">
+    </title>
+</head>
+<body>
+<div class="contenido" align="center">
+    <div class="navegacion">
+        <nav>
+            <a href=#>MI CUENTA</a>
+            <a href=#>EDITAR PERFIL</a>
+            <a href='logout.php'>CERRAR SESION</a>
+        </nav>
+    </div>
 <?php
-/**
- * Created by PhpStorm.
- * User: mauricio
- * Date: 03-10-2015
- * Time: 19:00
- */
 include_once('config/conexion.php');
 session_start();
 
 if(isset($_SESSION['sesion']))
 {
     $ses=$_SESSION['sesion'];
-    echo 'logueado como: '.$_SESSION['sesion'];
+    ?>
+<div class="saludo" align="right">
+    <?php echo 'logueado como: '.$_SESSION['sesion']; ?>
+</div>
+<?php
     $query=mysql_query("SELECT * FROM producto pr INNER JOIN usuario us USING (id_usuario) WHERE us.rut_usuario='".$ses."'");
     while ($reg=mysql_fetch_array($query)){
-        echo $reg['nombre_producto'];
+?>
+<div class="tabla">
+    <table>
+<?php        echo $reg['nombre_producto']; ?>
+    </table>
+</div>
+<?php
     }
 }else {
     echo "<script>alert('Su session ha expirado')</script>";
     header('refresh:0;login.php');
 }
 mysql_close();
+?>
+</div>
+</body>
